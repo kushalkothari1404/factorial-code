ca5716828ab6f5373ba355f66caeb5bef58f9bbd
kushalkothari1404 <168670419+kushalkothari1404@users.noreply.github.com>
2024-06-30 11:34:06 +0530

    Update factoial-code.cpp

--- factoial-code.cpp
+++ factoial-code.cpp
@@ -1,1 +1,21 @@
-
+#include<iostream>
+using namespace std;
+int factorial(int n)
+{
+  int fact = 1;
+  int i;
+  for(i=1;i<=n;i++)
+  {
+    fact = fact*i;
+  }
+  return fact;
+}
+int main()
+{
+  int n;
+  cout<<"Enter number: ";
+  cin>>n;
+  cout<<"The Factorial of "<<n<<" is:"<<factorial(n);
+  return 0;
+}
+  
